c0375abcf37de54f44119c1b0062c0a8c767167b
Michal Chomo <michal.chomo@email.cz>
2017-12-14 13:50:56 +0200

    refactor day 10

--- day10.py
+++ day10.py
@@ -1,49 +1,48 @@
-def knot_hash_round(lengths, circle):
-    global CIRCLE_LEN
-    global cur_pos
-    global skip_size
-    for length in lengths:
-        if (cur_pos + length) > CIRCLE_LEN:
-            sublist = circle[cur_pos:] + circle[:(cur_pos + length) % CIRCLE_LEN]
+def knot_hash_round(hash_input, circle, cur_pos, skip_size):
+    for h in hash_input:
+        if (cur_pos + h) > len(circle):
+            sublist = circle[cur_pos:] + circle[:(cur_pos + h) % len(circle)]
             sublist.reverse()
-            circle[cur_pos:] = sublist[:CIRCLE_LEN-cur_pos]
-            if length > CIRCLE_LEN:
-                circle_sublist_end = length - CIRCLE_LEN
+            circle[cur_pos:] = sublist[:len(circle)-cur_pos]
+            if h > len(circle):
+                circle_sublist_end = h - len(circle)
             else:
-                circle_sublist_end = length - (CIRCLE_LEN - cur_pos)
-            circle[:circle_sublist_end] = sublist[CIRCLE_LEN-cur_pos:]
+                circle_sublist_end = h - (len(circle) - cur_pos)
+            circle[:circle_sublist_end] = sublist[len(circle)-cur_pos:]
         else:
-            if length > 1:
-                sublist = circle[cur_pos:(cur_pos+length)]
+            if h > 1:
+                sublist = circle[cur_pos:(cur_pos+h)]
                 sublist.reverse()
-                circle[cur_pos:(cur_pos+length)] = sublist
-        cur_pos = (cur_pos + length + skip_size) % CIRCLE_LEN
+                circle[cur_pos:(cur_pos+h)] = sublist
+        cur_pos = (cur_pos + h + skip_size) % len(circle)
         skip_size += 1
-    return circle
+    return (circle, cur_pos, skip_size)
 
+def knot_hash(hash_input):
+    skip_size = 0
+    cur_pos = 0
+    circle = [i for i in range(256)]
+    hash_input_list = [ord(x) for x in hash_input] + [17, 31, 73, 47, 23]
+    for _ in range(64):
+        (circle, cur_pos, skip_size) = knot_hash_round(hash_input_list, circle, cur_pos, skip_size)
+    chunks = [circle[i*16 : i*16+16] for i in range(16)]
+    dense_hash = []
+    for chunk in chunks:
+        xored = 0
+        for num in chunk:
+            xored ^= num
+        dense_hash.append(xored)
+    return "".join("%02x"%num for num in dense_hash)
 
-with open("day10_input") as infile:
-    lengths = list(map(int, infile.readline().split(',')))
-CIRCLE_LEN = 256
-skip_size = 0
-cur_pos = 0
-circle = [i for i in range(CIRCLE_LEN)]
-circle = knot_hash_round(lengths, circle)
-print(circle[0] * circle[1])
+if __name__ == "__main__":
+    with open("day10_input") as infile:
+        lengths = list(map(int, infile.readline().split(',')))
+    skip_size = 0
+    cur_pos = 0
+    circle = [i for i in range(256)]
+    (circle, _, _) = knot_hash_round(lengths, circle, 0, 0)
+    print(circle[0] * circle[1])
 
-with open("day10_input") as infile:
-    lengths2 = [ord(x) for line in infile for x in line]
-lengths2 += [17, 31, 73, 47, 23]
-skip_size = 0
-cur_pos = 0
-circle = [i for i in range(CIRCLE_LEN)]
-for _ in range(64):
-    circle = knot_hash_round(lengths2, circle)
-chunks = [circle[i*16 : i*16+16] for i in range(16)]
-dense_hash = []
-for chunk in chunks:
-    xored = 0
-    for num in chunk:
-        xored ^= num
-    dense_hash.append(xored)
-print("".join("%02x"%num for num in dense_hash))
+    with open("day10_input") as infile:
+        lengths2 = infile.readline().strip()
+    print(knot_hash(lengths2))
