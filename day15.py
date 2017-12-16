51665d25965b7e9a93e13877e9d26f1b12c8eb02
Michal Chomo <michal.chomo@email.cz>
2017-12-16 11:43:34 +0200

    refactor day 15

--- day15.py
+++ day15.py
@@ -1,10 +1,12 @@
 mod = 2147483647
+fac_a = 16807
+fac_b = 48271
 total = 0
 val_a = 722
 val_b = 354
 for _ in range(40000000):
-    val_a = (val_a * 16807) % mod
-    val_b = (val_b * 48271) % mod
+    val_a = (val_a * fac_a) % mod
+    val_b = (val_b * fac_b) % mod
     if (val_a & 0xffff) == (val_b & 0xffff):
         total += 1
 print(total)
@@ -12,12 +14,12 @@ total = 0
 val_a = 722
 val_b = 354
 for _ in range(5000000):
-    val_a = (val_a * 16807) % mod
+    val_a = (val_a * fac_a) % mod
     while (val_a % 4) != 0:
         val_a = (val_a * 16807) % mod
-    val_b = (val_b * 48271) % mod
+    val_b = (val_b * fac_b) % mod
     while (val_b % 8) != 0:
-        val_b = (val_b * 48271) % mod
+        val_b = (val_b * fac_b) % mod
     if (val_a & 0xffff) == (val_b & 0xffff):
         total += 1
 print(total)
